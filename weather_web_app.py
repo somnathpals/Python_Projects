e754fac3fa3a1388c5992352f5413e70439d0a8a
somnathpals <68986063+somnathpals@users.noreply.github.com>
2025-11-29 22:38:10 +0530

    Add files via upload

--- weather_web_app.py
+++ weather_web_app.py
@@ -19,7 +19,7 @@ base_url = "http://api.weatherapi.com/v1/current.json"
 st.sidebar.header('Setting')
 
 unit = st.sidebar.selectbox("Temperature Unit: ",['Celsius', 'Fahrenheit'])
-days = st.sidebar.slider('Forecast days', min_value= 1, max_value= 7, value= 3)
+#days = st.sidebar.slider('Forecast days', min_value= 1, max_value= 7, value= 3)
 
 show_humidity = st.sidebar.checkbox('Show Humidity', value = True)
 show_wind = st.sidebar.checkbox('Show Wind Speed', value = True)
@@ -58,41 +58,41 @@ if st.button('Get Weather') and city:
         if show_wind:
             st.write(f' Wind Speed: {wind} kph')
         
-        st.markdown('---')
+#        st.markdown('---')
 
-        st.header(f' {days} - Days Forecast')
+#        st.header(f' {days} - Days Forecast')
         
-        forecast_day = data['forecast']['forecastday']
+#        forecast_day = data['forecast']['forecastday']
 
-        for day in forecast_day:
-            date = day['date']
-            if unit == 'Celsius':
-                min_temp = day['day']['mintemp_c']
-                max_temp = day['day']['maxtemp_c']
-            else:
-                min_temp = day['day']['mintemp_f']
-                max_temp = day['day']['maxtemp_f']
+#        for day in forecast_day:
+#            date = day['date']
+#            if unit == 'Celsius':
+#                min_temp = day['day']['mintemp_c']
+#                max_temp = day['day']['maxtemp_c']
+#            else:
+#                min_temp = day['day']['mintemp_f']
+#                max_temp = day['day']['maxtemp_f']
+#        
+#        condition = day['day']['condition']['text']
+#        icon_url = 'https:' + day['day']['condition']['icon']
+
+#        col1, col2, col3, col4 = st.columns([2,2,2,2])
+
+#        with col1:
+#            st.write((f' {date}'))
+
+#        with col2:
+#            st.image(icon_url, width= 50)
         
-        condition = day['day']['condition']['text']
-        icon_url = 'https:' + day['day']['condition']['icon']
-
-        col1,col2,col3,col4 = st.columns([2,2,2,2])
-
-        with col1:
-            st.write((f' {date}'))
-
-        with col2:
-            st.image(icon_url, width= 50)
+#        with col3:
+#            st.write(f' Min: {min_temp} {unit[0]}')
         
-        with col3:
-            st.write(f' Min: {min_temp} {unit[0]}')
+#        with col4:
+#            st.write(f' Max: {max_temp} {unit[0]}')
         
-        with col4:
-            st.write(f' Max: {max_temp} {unit[0]}')
-        
-        st.write(f' {condition}')
+#        st.write(f' {condition}')
 
-        st.markdown('---')
+#        st.markdown('---')
 
     else:
         st.error('City Not Found')
