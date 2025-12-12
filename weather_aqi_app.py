92ab5fa956c0aefe46c6ccbf5ec06f4fb1254dfe
somnathpals <68986063+somnathpals@users.noreply.github.com>
2025-12-12 21:51:04 +0530

    Add files via upload

--- weather_aqi_app.py
+++ weather_aqi_app.py
@@ -14,6 +14,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
+st.title('Weather + AQI Dashboard')
 
 # ============================================================
 # SESSION STATE
